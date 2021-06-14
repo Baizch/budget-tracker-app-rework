f5efbaee77febe765f6d8dcc8484e4f75d74c3e8
Bzch <francobaisch00@gmail.com>
2021-06-14 13:08:24 -0300

    set up initial server config

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+app.listen(3000, () => {
+  console.log('Server is up and running!');
+});
