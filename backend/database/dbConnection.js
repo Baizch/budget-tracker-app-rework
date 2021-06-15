f59f15f6a781c585beab06c5be7b27f1d16df94e
Bzch <francobaisch00@gmail.com>
2021-06-15 11:03:38 -0300

    harcoded data to access db & host info was removed

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'budget_db',
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 try {
