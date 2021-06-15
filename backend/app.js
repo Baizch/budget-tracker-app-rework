11a8400465d984ab1135421858303a0b3b685f81
Bzch <francobaisch00@gmail.com>
2021-06-15 11:50:22 -0300

    imported transactions route

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 db = require('./database/dbConnection');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const transactionsRouter = require('./routes/transactions');
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/transactions', transactionsRouter);
 
 try {
   app.listen(3000, () => {
