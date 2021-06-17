ed18b6d396cee655fc34fd6157f2dedeafef3100
Bzch <francobaisch00@gmail.com>
2021-06-17 11:43:41 -0300

    created transactions model & run migrations

---
+++ backend/migrations/20210617143407-create-transactions.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      concepto: {
+        type: Sequelize.STRING
+      },
+      monto: {
+        type: Sequelize.INTEGER
+      },
+      fecha: {
+        type: Sequelize.DATE
+      },
+      tipo: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  }
+};
