3847d3f21ca0632420cbddd4467ad4629564eeb0
Bzch <francobaisch00@gmail.com>
2021-06-18 13:28:03 -0300

    added function to get transaction by id

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -13,8 +13,30 @@ const getTransactions = async (req, res) => {
   }
 };
 
+const getTransaction = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const transaction = await Transactions.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+    if (transaction) {
+      res.status(200).json(transaction);
+    } else {
+      res.status(404).json({ err: 'Transaction not found' });
+    }
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
 const transactionsController = {
   getTransactions,
+  getTransaction,
 };
 
 module.exports = transactionsController;
