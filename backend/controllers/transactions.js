2ee4fbfc82541da897b03cffcf55f36e140cf540
Bzch <francobaisch00@gmail.com>
2021-06-18 11:56:16 -0300

    excluded fields createdAt, updatedAt

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -2,7 +2,11 @@ const { Transactions } = require('../models/index');
 
 const getTransactions = async (req, res) => {
   try {
-    const transactions = await Transactions.findAll();
+    const transactions = await Transactions.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
     res.status(200).json(transactions);
   } catch (err) {
     res.status(400).json({ err: err.message });
