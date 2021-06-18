7d607dbdf85a7b5c745faeed998e5c8880db7ff6
Bzch <francobaisch00@gmail.com>
2021-06-18 13:28:22 -0300

    created route to get transaction by id

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,12 @@
 const transactionsRouter = require('express').Router();
-const { getTransactions } = require('../controllers/transactions');
+const {
+  getTransactions,
+  getTransaction,
+} = require('../controllers/transactions');
 const notEmpty = require('../middlewares/fieldNotEmpty');
 
 transactionsRouter.get('/', getTransactions);
+
+transactionsRouter.get('/:id', getTransaction);
 
 module.exports = transactionsRouter;
