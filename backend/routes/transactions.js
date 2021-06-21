2eee576a80370f06b4b37494d6b9f344cd712117
Bzch <francobaisch00@gmail.com>
2021-06-21 11:09:56 -0300

    added delete route

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -2,11 +2,20 @@ const transactionsRouter = require('express').Router();
 const {
   getTransactions,
   getTransaction,
+  deleteTransaction,
 } = require('../controllers/transactions');
 const notEmpty = require('../middlewares/fieldNotEmpty');
 
+//ROUTES
+
+//GET routes
 transactionsRouter.get('/', getTransactions);
 
 transactionsRouter.get('/:id', getTransaction);
+
+//DEL route
+transactionsRouter.delete('/:id', deleteTransaction);
+
+//POST & PUT routes
 
 module.exports = transactionsRouter;
